3c9bb672105a3cda70649af6df4b9b2613fea0db
prishsha <prishavadhavkar@gmail.com>
2026-02-17 21:15:16 +0530

    headerfunc

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 from crawler import crawl
 from sqlScanner import scan_sql_injection
 from xssScanner import scan_xss
+from headerScanner import scan_headers
 
 def main():
-    print("\n")
     print("   WebSpidey Vulnerability Scanner  ")
     print("\n")
 
     target_url = input("Enter target URL: ").strip()
 
+    # crawling
     print("\n[+] Crawling target website...")
     urls = crawl(target_url)
-
     print(f"[+] Total URLs discovered: {len(urls)}\n")
 
-    # SQL Injection 
+    # sql injection
     print("[+] Running SQL Injection scan...")
     sqli_results = scan_sql_injection(urls)
 
-    # XSS 
+    # xss
     print("[+] Running XSS scan...")
     xss_results = scan_xss(urls)
 
-    # REPORT 
-    print("\nSCAN REPORT\n")
+    # headers
+    print("[+] Checking Security Headers...")
+    header_results = scan_headers(target_url)
 
-    # SQL Injection Report
+    # report
+    print("\nFINAL SCAN REPORT\n")
+
+    # SQL Injection
     print("[SQL Injection]")
     if sqli_results:
         print("Status: VULNERABLE")
@@ -34,9 +38,9 @@ def main():
     else:
         print("Status: NOT DETECTED")
 
-    print("\n\n")
+    print("\n")
 
-    # XSS Report
+    # XSS
     print("[Cross-Site Scripting (XSS)]")
     if xss_results:
         print("Status: VULNERABLE")
@@ -44,6 +48,17 @@ def main():
             print(" -", url)
     else:
         print("Status: NOT DETECTED")
+
+    print("\n")
+
+    # Headers
+    print("[Security Headers]")
+    if header_results:
+        print("Missing Headers")
+        for header in header_results:
+            print(" -", header)
+    else:
+        print("All Required Headers Present")
 
     print("\n")
 
